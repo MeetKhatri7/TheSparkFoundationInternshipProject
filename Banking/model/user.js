5c394f3d8e9211d9c56475889a4d922a93b2906c
Meet Khatri <khatrimeet13@gmail.com>
2023-09-21 15:13:27 +0530

    initial commit

---
+++ Banking/model/user.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  currentbal: {
+    required: true,
+    type: Number,
+  },
+});
+
+export const User = mongoose.model("User", schema);
