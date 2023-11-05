c361426c25d9a51aac42c789d21cb991ef3e018a
MeetKhatri7 <129852118+MeetKhatri7@users.noreply.github.com>
2023-11-05 16:45:24 +0530

    Delete Banking directory

--- Banking/routes/transaction.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getalltransactions , addNewTransaction } from "../controllers/transaction.js";
-const router = express.Router();
-
-router.get("/alltransaction" , getalltransactions);
-router.post("/transfer" , addNewTransaction);
-
-export default router;
--- Banking/server.js
+++
@@ -1,8 +0,0 @@
-import {app} from "./app.js";
-import { connectDB } from "./data/database.js";
-
-connectDB();
-
-app.listen(5000, () => {
-  console.log(`Server is working on port` );
-});
